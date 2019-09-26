3202bea4f32ae887c69a70a9bacb940f60612955
Jesse Sosmena <diximobureros@gmail.com>
2019-09-26 21:56:11 +0800

    Drop: remove all block comments

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 $("document").ready(function() {
 
-/**
- * Will return message with passed in parameter.
- * @param {string} [name="You"]
- */
 function message() {
   return `Hello World!`;
 }
 
-/**
- * Will find #root element and set HTML to "Hello World!".
- */
 function editDom() {
-  const root = $('#root'); // Get root element.
+  const root = $('#root'); 
   if (root) {
     root.html(`
 
@@ -26,11 +19,11 @@ function editDom() {
 		    </div>
 		</div>
 
-    `); // Set html of the root element.
+    `); 
   }
 }
 
-editDom(); // Call editDom.
+editDom(); 
 
 });
 
